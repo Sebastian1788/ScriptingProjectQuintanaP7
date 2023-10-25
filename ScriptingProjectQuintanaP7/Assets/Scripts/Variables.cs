e5157b6c7acd8f2e964e4a0253cb31b53f4f4a60
Sebastian1788 <145072446+Sebastian1788@users.noreply.github.com>
2023-10-25 14:51:54 -0700

    Unity
    I added more scripts.

---
+++ ScriptingProjectQuintanaP7/Assets/Scripts/Variables.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt):
+        
+    }
+}
+
+int MultiplyByTwo(int number)
+
+    int result;
+result = NumberFormatInfo * 2;
+return result;
