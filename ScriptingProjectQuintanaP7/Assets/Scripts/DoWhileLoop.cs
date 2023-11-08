7eb5bd28df81bb0516d6fba9199a00370c4cacf3
Sebastian1788 <145072446+Sebastian1788@users.noreply.github.com>
2023-11-08 15:29:32 -0800

    coding
    I finished scripting.

---
+++ ScriptingProjectQuintanaP7/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+
+        }
+
+    // Update is called once per frame
+    void Update()
+
+
+
+        }While(shouldContinue == true);
+    }
+}
